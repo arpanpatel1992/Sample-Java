9aa60588cc852fd90e6680314ab861188dc3668d
Arpan Patel <arpanpatel1992@gmail.com>
2016-07-01 11:32:20 -0400

    Not changed

---
+++ academic/src/main/java/com/arpan/entities/Contact.java
@@ -0,0 +1,54 @@
+package com.arpan.entities;
+
+
+/**
+ * @author arpan
+ *
+ */
+public class Contact {
+
+	private Long id;
+	
+	private String name;
+	
+	private Long addressId;
+
+	public Contact() {
+		
+	}
+	
+	
+	public Contact(Long id, String name, Long addressId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.addressId = addressId;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+	
+	
+}
